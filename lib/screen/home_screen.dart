22ee6cfcb22c25cc269cb18676ef0ee2a4761c44
Fauzi <macbookpro@Fuzzy-2.local>
2022-12-15 03:17:04 +0800

    redesing UI and fix bugs

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -15,68 +15,110 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final userInfo = FirebaseAuth.instance.currentUser!;
-  TextEditingController controller = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController birthdayController = TextEditingController();
+  final validationKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: TextFormField(
-          controller: controller,
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                final name = controller.text;
-
-                if (name.isNotEmpty) {
-                  createUser(name: name);
-                  controller.clear();
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      backgroundColor: Colors.red,
-                      content: Text('Please enter name')));
-                }
-              },
-              icon: const Icon(Icons.add))
-        ],
-      ),
+      appBar: AppBar(),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Signed In as'),
-              Text(
-                userInfo.email!,
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50)),
-                  onPressed: () => FirebaseAuth.instance.signOut(),
-                  icon: const Icon(Icons.logout_outlined),
-                  label: const Text('Sign Out'))
-            ],
+          child: Form(
+            key: validationKey,
+            autovalidateMode: AutovalidateMode.disabled,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: nameController,
+                  validator: (value) {
+                    if (value != null) {
+                      return null;
+                    } else {
+                      return 'Please enter name';
+                    }
+                  },
+                  decoration: const InputDecoration(labelText: 'name'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value != null) {
+                      return null;
+                    } else {
+                      return 'Please enter age';
+                    }
+                  },
+                  controller: ageController,
+                  decoration: const InputDecoration(labelText: 'age'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value != null) {
+                      return null;
+                    } else {
+                      return 'Please enter birthday';
+                    }
+                  },
+                  controller: birthdayController,
+                  decoration: const InputDecoration(labelText: 'birthday'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(50)),
+                    onPressed: () {
+                      if (validationKey.currentState!.validate()) {
+                        final users = UserModel(
+                            name: nameController.text,
+                            age: int.parse(ageController.text),
+                            birthday: DateTime.parse(birthdayController.text));
+                        createUser(users);
+                        nameController.clear();
+                        ageController.clear();
+                        birthdayController.clear();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                backgroundColor: Colors.red,
+                                content: Text('Please enter name')));
+                      }
+                    },
+                    icon: const Icon(Icons.logout_outlined),
+                    label: const Text('Sign Out'))
+                // ElevatedButton.icon(
+                //     style: ElevatedButton.styleFrom(
+                //         minimumSize: const Size.fromHeight(50)),
+                //     onPressed: () => FirebaseAuth.instance.signOut(),
+                //     icon: const Icon(Icons.logout_outlined),
+                //     label: const Text('Sign Out'))
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Future createUser({required String name}) async {
+  Future createUser(UserModel user) async {
     var uuid = const Uuid();
 
     //Reference to the document
     final docUser =
         FirebaseFirestore.instance.collection('users').doc(uuid.v4());
-    final json = UserModel(
-        id: docUser.id, name: name, age: 21, birthday: DateTime.now());
-    final jsonResponse = json.toJson();
+    user.id = docUser.id;
+    final jsonResponse = user.toJson();
     //create new document and write new data to firestore
     await docUser.set(jsonResponse);
   }
